5dea91bf043a1c83e65af2e421479b40af74faf0
Andreas Böhm <andreas@boehm.cx>
2014-11-03 09:38:06 +0100

    move remote-codes to reusable header-file, fix compiler warnings

--- Harmony_RF_Outlet/Harmony_RF_Outlet.ino
+++ Harmony_RF_Outlet/Harmony_RF_Outlet.ino
@@ -6,46 +6,42 @@
  * You need to add a 'One-For-All HC8300 Light Control'-Device to your Harmony,
  * because the IR-codes from this devices remote are used here.
  */
+#include "OneForAllHc8300RemoteCodes.h"
+
+// Libraries
 #include <RCSwitch.h>
 #include <IRremote.h>
-
 #define RCSwitchDisableReceiving
-#define IR_PIN 2
-#define RF_PIN 4
+
+// PIN configuration
+#define IR_SENSOR_PIN 2
+#define RF_TRANSMITTER_PIN 4
 
 /**
  * RF Socket Configuration
  */
-char* rfSystemCode = "00001";
-char* rfSocket1 = "01000";
-
-/**
- * IR Code Configuration
- */
-const unsigned long socket1On = 3895178441;
-const unsigned long socket1Off = 254685380;
-const unsigned long socket2On = 326842813;
-const unsigned long socket2Off = 981317048;
+char rfSystemCode[] = "00001";
+char rfSocket1[] = "01000";
 
 decode_results results;
 
-IRrecv irrecv(IR_PIN);
+IRrecv irrecv(IR_SENSOR_PIN);
 RCSwitch rcSwitch = RCSwitch();
 
 void setup() {
   irrecv.enableIRIn();
   irrecv.blink13(1);
-  rcSwitch.enableTransmit(RF_PIN);
+  rcSwitch.enableTransmit(RF_TRANSMITTER_PIN);
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
 
     switch(results.value) {
-      case socket1On:
+      case HC8300_1ON:
         rcSwitch.switchOn(rfSystemCode, rfSocket1);
         break;
-      case socket1Off:
+      case HC8300_1OFF:
         rcSwitch.switchOff(rfSystemCode, rfSocket1);
         break;
       default:
---
+++ Harmony_RF_Outlet/OneForAllHc8300RemoteCodes.h
@@ -0,0 +1,8 @@
+/*
+ * All IR-Codes sent by 'One-For-All HC8300 Light Control'-Remote
+ */
+
+#define HC8300_1OFF 254685380UL
+#define HC8300_1ON  3895178441UL
+#define HC8300_2OFF 981317048UL
+#define HC8300_2ON  326842813UL
